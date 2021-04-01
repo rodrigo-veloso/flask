3adefd7a3c9562dc5e99bb8cd41dfc0504928c58
Rodrigo <rodrigoveloso1t@gmail.com>
2021-04-01 13:40:02 -0300

    1

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, request, redirect, url_for, flash, jsonify
+
+import functools
+import os
+
 import numpy as np
-import pandas as pd
+import tensorflow as tf
+import tensorflow_hub as hub
+import matplotlib.pylab as plt
+
 from joblib import load
 import json
 import logging
@@ -10,27 +17,64 @@ logging.getLogger().setLevel(logging.INFO)
 
 app = Flask(__name__)
 
-def predict_new(X, probs=True):
-    model = load('titanic_model_rf.pkl')
-    p = model.get_preprocessing()
-    
-    X = p.clean_data(X)
-    X = p.categ_encoding(X)
-    
-    columns = model.get_columns()
-    for col in columns:
-        if col not in X.columns:
-            X[col] = 0
-    if probs:
-        return model.predict_proba(X)[:,1]
-    else:
-        return model.predict(X)
+def crop_center(image):
+  """Returns a cropped square image."""
+  shape = image.shape
+  new_shape = min(shape[1], shape[2])
+  offset_y = max(shape[1] - shape[2], 0) // 2
+  offset_x = max(shape[2] - shape[1], 0) // 2
+  image = tf.image.crop_to_bounding_box(
+      image, offset_y, offset_x, new_shape, new_shape)
+  return image
+
+@functools.lru_cache(maxsize=None)
+def load_image(image_url, image_size=(256, 256), preserve_aspect_ratio=True):
+  """Loads and preprocesses images."""
+  # Cache image file locally.
+  image_path = tf.keras.utils.get_file(os.path.basename(image_url)[-128:], image_url)
+  # Load and convert to float32 numpy array, add batch dimension, and normalize to range [0, 1].
+  img = plt.imread(image_path).astype(np.float32)[np.newaxis, ...]
+  if img.max() > 1.0:
+    img = img / 255.
+  if len(img.shape) == 3:
+    img = tf.stack([img, img, img], axis=-1)
+  img = crop_center(img)
+  img = tf.image.resize(img, image_size, preserve_aspect_ratio=True)
+  return img
+
+def apply_model()
+
+    content_image_url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Golden_Gate_Bridge_from_Battery_Spencer.jpg/640px-Golden_Gate_Bridge_from_Battery_Spencer.jpg'  # @param {type:"string"}
+    style_image_url = 'https://upload.wikimedia.org/wikipedia/commons/0/0a/The_Great_Wave_off_Kanagawa.jpg'  # @param {type:"string"}
+
+    output_image_size = 384  # @param {type:"integer"}
+
+    # The content image size can be arbitrary.
+    content_img_size = (output_image_size, output_image_size)
+    # The style prediction model was trained with image size 256 and it's the 
+    # recommended image size for the style image (though, other sizes work as 
+    # well but will lead to different results).
+    style_img_size = (256, 256)  # Recommended to keep it at 256.
+
+    content_image = load_image(content_image_url, content_img_size)
+    style_image = load_image(style_image_url, style_img_size)
+    style_image = tf.nn.avg_pool(style_image, ksize=[3,3], strides=[1,1], padding='SAME')
+    # Load TF-Hub module.
+
+    hub_handle = 'https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2'
+    hub_module = hub.load(hub_handle)
+
+    # Stylize content image with given style image.
+    # This is pretty fast within a few milliseconds on a GPU.
+
+    outputs = hub_module(tf.constant(content_image), tf.constant(style_image))
+    stylized_image = outputs[0]
 
 @app.route('/invocations', methods=['POST'])
 def predict():
-    data = pd.read_json(request.json)
-    predictions = np.array2string(predict_new(data, probs=True))
-    return jsonify(predictions)
+    #data = pd.read_json(request.json)
+    predictions = apply_model()#np.array2string(predict_new(data, probs=True))
+    return predictions
 
 @app.route('/health', methods=['GET'])
 def health_check():
